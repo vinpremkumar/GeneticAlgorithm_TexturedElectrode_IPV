401d82dd24ecedbf6f59e33ce5128f507d155814
Premkumar Vincent <49431830+vinpremkumar@users.noreply.github.com>
2020-02-02 20:06:10 +0900

    Edited the files GenerateRegularPolygon and MeshPolygon
    Completed coding of MeshPolygon with comments.
    Edited GenerateRegularPolygon to re-generate if the polygon initially generated was not balanced.

--- GenerateRegularPolygon.m
+++ GenerateRegularPolygon.m
@@ -1,5 +1,5 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Creates the polygon by sampling points on a Ellipse.
+% Creates a 2D polygon by sampling points on a Ellipse.
 %
 % Params:
 %     aveRadius   - The average radius of the ellipse inside which the polygon will be plotted
@@ -13,21 +13,25 @@
 function [x_vertices, y_vertices] = GenerateRegularPolygon ( aveRadius, numVerts, AspectRatio )
 
 % generate n angle steps
+% Make number of angles equal to number of vertices
 angleSteps(1:numVerts) = (2*pi / numVerts);
 
 % now generate the points
 angle = unifrnd(0, 1);
 angle(2:numVerts+1) = angleSteps(1:numVerts);
-angle = cumsum(angle);
-angle(end) = [];
+angle = cumsum(angle); % Incrementing angle per vertex
+angle(end) = []; % We wouldnt need the last angle since it is a closed polygon with a flat base
 
-x_vertices = round((1/AspectRatio)*(aveRadius*cos(angle)));
+% Using polar cordinate equation for an ellipse
+% The AspectRatio variables gives the ratio of a/b where a and b are vertical axis and horizontal radius lengths.
+x_vertices = round((1/AspectRatio)*(aveRadius*cos(angle))); 
 y_vertices = round(aveRadius*sin(angle));
     
-    
+% Close the polygon by making its end vertex = start vertex    
 x_vertices(end+1) = x_vertices(1);
 y_vertices(end+1) = y_vertices(1);
 
+% Make the base of the polygon flat so that it can stand upright on a surface
 index = find(y_vertices==min(y_vertices), 1 );
 if index == numVerts
     y_vertices(index-1) = y_vertices(index);
@@ -35,31 +39,21 @@ else
     y_vertices(index+1) = y_vertices(index);
 end
 
+%% Check for center of gravity
 y_vertices = y_vertices - min(y_vertices);
 
 [x_centroid, ~] = polygoncentroid(x_vertices,y_vertices);
 
 CenterOfMass_balanced = nnz(x_centroid >= min(x_vertices(y_vertices == min(y_vertices))) & x_centroid <= max(x_vertices(y_vertices == min(y_vertices))));
-if CenterOfMass_balanced == 1
-    fprintf("\nBalanced structure\n");
-else
-    fprintf("\nUnBalanced structure\n");
+
+% Re-run of the structure's center of gravity does not fall within the base
+if CenterOfMass_balanced ~= 1
+    GenerateRegularPolygon ( aveRadius, numVerts, AspectRatio )
 end
 
 end
 
-function output =  clip(x, min, max)
-if min > max
-    output = x;
-elseif x < min
-    output = min;
-elseif x > max
-    output = max;
-else
-    output = x;
-end
-end
-
+%% This function finds the centroid (Center of gravity) of the 2D polygon
 function [xc,yc] = polygoncentroid(x,y)
 xn = circshift(x,-1);
 yn = circshift(y,-1);
--- MeshPolygon.m
+++ MeshPolygon.m
@@ -1,15 +1,30 @@
-Radius = 100;
-Num_of_Vertices = 4;
-Aspect_ratio = 1;
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Mesh the 2D polygon into NumOfMeshes x NumOfMeshes.
+% The output cells containes vertices in each mesh box.
+% 
+% Params:
+%     x,y                 - Outputs of GenerateRegularPolygon function.
+%     quadrant_x_vertices - x vertices sorted into their respective meshgrids. 
+%     quadrant_y_vertices - y vertices sorted into their respective meshgrids.
+%
+% Returns cells as [quadrant_x_vertices, quadrant_y_vertices] which contain 
+% the x and y vertices sorted into their respective meshgrids.
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-[x,y] = GenerateRegularPolygon (Radius, Num_of_Vertices, Aspect_ratio)
+function [quadrant_x_vertices, quadrant_y_vertices] = MeshPolygon(x,y)
+%% Testing purpose only
+% Radius = 100;
+% Num_of_Vertices = 7;
+% Aspect_ratio = 1;
+%
+% [x,y] = GenerateRegularPolygon (Radius, Num_of_Vertices, Aspect_ratio)
 
+%% Setting mesh grid spacing along x and y axes
 x_spacing = round(linspace(min(x),max(x),6));
 y_spacing = round(linspace(max(y),min(y),6));
+
+%% Plot (for testing purpose only)
 [X_spacing, Y_spacing] = meshgrid(x_spacing,y_spacing);
-
-
-%% Plot
 plot(x,y,'r-x');
 y_AspectRatio = (max(y)- min(y))/(max(x) - min(x));
 pbaspect([1 y_AspectRatio 1])
@@ -17,13 +32,42 @@ hold on
 plot(X_spacing,Y_spacing,'--b',X_spacing',Y_spacing','--b')
 hold off
 
-%%
-for i = 1:4
-    for j = 1:4
+%% Allocate vertices their grid cell location
+% Delete the last vertex as it is same as the first vertex
+x(end) = [];
+y(end) = [];
+
+% Set the number of grids (NumOfMeshes x NumOfMeshes)
+NumOfMeshes = 5;
+
+% Preallocate memory for cells
+quadrant_x_vertices = cell(NumOfMeshes,NumOfMeshes);
+quadrant_y_vertices = cell(NumOfMeshes,NumOfMeshes);
+
+% Place vertices in the specific meshgrid cell
+for i = 1:NumOfMeshes
+    for j = 1:NumOfMeshes
         
-        index_y = (y <= y_spacing(i) & y > y_spacing(i+2))
-        index_x = (x >= x_spacing(j) & x < x_spacing(j+2))
-        index = index_y & index_x ;
+        if (i == 1 || i == NumOfMeshes) && (j == 1 || j == NumOfMeshes)
+            index_y = (y <= y_spacing(i) & y >= y_spacing(i+1));
+            index_x = (x >= x_spacing(j) & x <= x_spacing(j+1));
+            index = index_y & index_x ;
+            
+            
+        elseif  i == NumOfMeshes || j == NumOfMeshes
+            index_y = (y < y_spacing(i) & y >= y_spacing(i+1));
+            index_x = (x > x_spacing(j) & x <= x_spacing(j+1));
+            index = index_y & index_x ;
+        else
+            index_y = (y <= y_spacing(i) & y > y_spacing(i+1));
+            index_x = (x >= x_spacing(j) & x < x_spacing(j+1));
+            index = index_y & index_x ;
+        end
+        
+        quadrant_x_vertices{i,j} = x(index);
+        quadrant_y_vertices{i,j} = y(index);
         
     end
+end
+
 end
