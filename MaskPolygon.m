53a6e68d2dd70eb6947d60aecb1ecffc45d86895
Premkumar Vincent <49431830+vinpremkumar@users.noreply.github.com>
2020-02-04 22:16:09 +0900

    Minor changes
    Function header comments added

--- MaskPolygon.m
+++ MaskPolygon.m
@@ -1,5 +1,19 @@
-function binaryImage = MaskPolygon(x,y,Radius)
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Masks the 2D polygon in a static reference plane of size 2*Radius x 2*Radius.
+% This will convert the vertices into a solid 2D polygon
+% The polygon is meshed over the reference plane and is indicated by logic 1, 
+% while the empty area is indicated by logic 0
+% 
+% Params:
+%     Radius      - The average radius of the ellipse inside which the polygon will be plotted
+%                   This is obtained from GenerateRegularPolygon.m
+%     x,y         - Vertices of the 2D polygon. 
+%     binaryImage - Masked image of the 2D polygon. Size: 2*Radius x 2*Radius
+%
+% Returns binaryImage (1 = area covered by polygon, 0 = uncovered area).
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+function binaryImage = MaskPolygon(x,y,Radius)
 %% Testing purpose only
 % clear all;
 % 
@@ -19,10 +33,9 @@ x = x + adjust_factor;
 binaryImage = flip(poly2mask(x,y, 2*Radius, 2*Radius));
 
 %% Testing purpose only
-figure(2)
-imshow(binaryImage);
-%%
-
+% figure(2)
+% imshow(binaryImage);
+%%%
 
 end
 
--- polygoncentroid.m
+++ polygoncentroid.m
@@ -1,4 +1,10 @@
-%% This function finds the centroid (Center of gravity) of the 2D polygon
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% This function finds the centroid (Center of gravity) of the 2D polygon
+% 
+% It was answered by Bruno Luong to a question in Mathworks's forum
+% Link: https://www.mathworks.com/matlabcentral/answers/469852-how-do-i-find-the-centre-of-gravity-for-an-irregular-shape#answer_383244
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 function [xc,yc] = polygoncentroid(x,y)
 xn = circshift(x,-1);
 yn = circshift(y,-1);
