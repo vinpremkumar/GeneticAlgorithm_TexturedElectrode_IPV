e07c15943a125ec0230cea7be514eacbe81f1d26
Premkumar Vincent <49431830+vinpremkumar@users.noreply.github.com>
2020-02-06 19:02:23 +0900

    Vectorized
    Vectorized the code for ReconstructPolygon.m

--- ReconstructPolygon.m
+++ ReconstructPolygon.m
@@ -2,11 +2,14 @@ function thisBoundary = ReconstructPolygon(binaryImage)
 
 figure(6)
 hold on
-boundaries = bwboundaries(flip(binaryImage),4,'holes');
+boundaries = bwboundaries(flip(binaryImage));
 numberOfBoundaries = size(boundaries, 1);
-for k = 1 : numberOfBoundaries;
-thisBoundary = boundaries{k};
-plot(thisBoundary(:,2), thisBoundary(:,1), 'g', 'LineWidth', 2);
-end
+% for k = 1 : numberOfBoundaries
+% thisBoundary = boundaries{k};
+% plot(thisBoundary(:,2), thisBoundary(:,1), 'g', 'LineWidth', 2);
+arrayfun(@(x) plot(x{:}(:,2),x{:}(:,1)),boundaries)
+xlim ([0, size(binaryImage,1)])
+ylim ([0, size(binaryImage,1)])
+% end
 hold off
 end
